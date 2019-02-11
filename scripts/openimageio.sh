56c29229ba6372581187021afac8738141e4605f
kokodakdak <kokodak95@naver.com>
2019-02-11 14:38:28 +0900

    install OpenColorIO

--- scripts/openimageio.sh
+++ scripts/openimageio.sh
@@ -4,8 +4,9 @@ if [[$EUID -ne 0 ]]; then
 	exit 1
 fi
 
+yum -y install OpenColorIO
 yum -y install OpenImageIO
 yum -y install OpenImageIO-iv
 yum -y install OpenImageIO-devel
-yum -y  install OpenImageIO-utils
+yum -y install OpenImageIO-utils
 yum -y install python-OpenImageIO
