663370be9a1af2004ef999e648306f6ec7daa4b6
kokodakdak <kokodak95@naver.com>
2019-02-13 09:18:19 +0900

    warning제거

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -4,12 +4,13 @@ cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 
-pip install --upgrade setuptools
+#pip install --upgrade setuptools #간혹 이 작업 이후 부팅이 안되는 컴퓨터가 발생함.
 pip install pymediainfo
 pip install --user PySide2
 pip install --user numpyyum -y install glibc
 pip install --user Cython
 pip install --user openpyxl
+pip install --user --upgrade lxml #openpyxl 사용시lxml 버전이 낮아서 뜨는 경고를 해결
 pip install --user xlsxwriter
 pip install --user xlrd
 pip install --user xlwt
